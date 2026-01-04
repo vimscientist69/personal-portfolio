57898da1cc3a238cb80f493922e1388e4f3d04c3
William <vimscientist69@gmail.com>
2026-01-04 16:31:20 +0200

    fix: removed hire me section

--- src/constants/index.js
+++ src/constants/index.js
@@ -34,10 +34,10 @@ export const navLinks = [
         id: "contact",
         title: "Contact",
     },
-    {
-        id: "hire",
-        title: "Hire Me",
-    },
+    // {
+    //     id: "hire",
+    //     title: "Hire Me",
+    // },
     {
         title: "Blog",
         id: "blog",
